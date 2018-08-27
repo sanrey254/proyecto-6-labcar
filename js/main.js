2644c2f1597e34fb8d27394a35b7d6113d49761c
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-08-27 13:24:11 -0500

    Agregando imagen mapa

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // La funcionalidad de tu proyecto
+let mymap = L.map('map').setView([19.4194865, -99.1916445], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2FucmV5MjU0IiwiYSI6ImNqa2s0ZjhsdzBlaXYzcHBqdmQ2eXdjOWcifQ.82D_7oPbwvYFF2IQq1-_NQ', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1Ijoic2FucmV5MjU0IiwiYSI6ImNqa2s0ZjhsdzBlaXYzcHBqdmQ2eXdjOWcifQ.82D_7oPbwvYFF2IQq1-_NQ'
+}).addTo(mymap);
